c2b485f4bc1341f93070563e8fa0bf33bb3bdfdd
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-09-13 00:50:48 -0400

    Add CLI to generate graph

---
+++ src/cli.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import * as program from "commander";
+import * as path from "path";
+import * as fs from "fs";
+
+import { generateDepsGraph } from "./index";
+
+program
+  .arguments("<dir>")
+  .action(
+    (dir): void => {
+      const sassFolder = path.join(process.cwd(), dir);
+      if (!isFolder(sassFolder)) {
+        console.log("Given argument should be a folder.");
+        return process.exit(1);
+      }
+
+      generateDepsGraph(sassFolder);
+    },
+  )
+  .parse(process.argv);
+
+function isFolder(folder: string): boolean {
+  return fs.existsSync(folder) && fs.lstatSync(folder).isDirectory();
+}
