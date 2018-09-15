4cc7ba6131e916c5031d99f7fa3ed33e4f529019
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-09-15 14:30:37 -0400

    Refactor tests to highlight the happy path

--- src/generation/sass-graph.test.ts
+++ src/generation/sass-graph.test.ts
@@ -3,12 +3,39 @@ import * as sassGraph from "sass-graph";
 import { Graph } from "../graph";
 import { sassGraphGraphToGraph } from "./sass-graph";
 
-it("return empty graph if sass graph has empty index", () => {
+describe("sass graph without index", () => {
+  it("return empty graph", () => {
+    const dir = "path/to/dir";
+    const emptyIndexSassGraph: sassGraph.Graph = {
+      dir,
+      extensions: ["scss", "sass"],
+      index: {},
+      loadPaths: ["path/to/load"],
+      addFile: () => {},
+      visitAncestors: () => {},
+      visitDescendents: () => {},
+      visit: () => {},
+    };
+
+    const graph = sassGraphGraphToGraph(emptyIndexSassGraph);
+
+    const emptyGraph = new Graph();
+    expect(graph).toEqual(emptyGraph);
+  });
+});
+
+it("return graph for 1 import", () => {
   const dir = "path/to/dir";
-  const emptyIndexSassGraph: sassGraph.Graph = {
+  const sassGraphWithIndex: sassGraph.Graph = {
     dir,
     extensions: ["scss", "sass"],
-    index: {},
+    index: {
+      [`${dir}/_child.scss`]: {
+        imports: [],
+        importedBy: [`${dir}/parent.scss`],
+        modified: "2018-01-01T00:00:00.000Z",
+      },
+    },
     loadPaths: ["path/to/load"],
     addFile: () => {},
     visitAncestors: () => {},
@@ -16,90 +43,63 @@ it("return empty graph if sass graph has empty index", () => {
     visit: () => {},
   };
 
-  const graph = sassGraphGraphToGraph(emptyIndexSassGraph);
+  const graph = sassGraphGraphToGraph(sassGraphWithIndex);
 
-  const emptyGraph = new Graph();
-  expect(graph).toEqual(emptyGraph);
+  const expectedGraph = new Graph();
+  expectedGraph.addVertice("parent", "_child");
+  expect(graph).toEqual(expectedGraph);
 });
 
-describe("sass graph with index", () => {
-  it("return graph for 1 import", () => {
-    const dir = "path/to/dir";
-    const sassGraphWithIndex: sassGraph.Graph = {
-      dir,
-      extensions: ["scss", "sass"],
-      index: {
-        [`${dir}/_child.scss`]: {
-          imports: [],
-          importedBy: [`${dir}/parent.scss`],
-          modified: "2018-01-01T00:00:00.000Z",
-        },
+it("return graph for a basic import tree", () => {
+  const dir = "path/to/dir";
+  const sassGraphWithIndex: sassGraph.Graph = {
+    dir,
+    extensions: ["scss", "sass"],
+    index: {
+      [`${dir}/_base.scss`]: {
+        imports: [],
+        importedBy: [`${dir}/main.scss`],
+        modified: "2018-01-01T00:00:00.000Z",
       },
-      loadPaths: ["path/to/load"],
-      addFile: () => {},
-      visitAncestors: () => {},
-      visitDescendents: () => {},
-      visit: () => {},
-    };
-
-    const graph = sassGraphGraphToGraph(sassGraphWithIndex);
-
-    const expectedGraph = new Graph();
-    expectedGraph.addVertice("parent", "_child");
-    expect(graph).toEqual(expectedGraph);
-  });
-
-  it("return graph for a basic import tree", () => {
-    const dir = "path/to/dir";
-    const sassGraphWithIndex: sassGraph.Graph = {
-      dir,
-      extensions: ["scss", "sass"],
-      index: {
-        [`${dir}/_base.scss`]: {
-          imports: [],
-          importedBy: [`${dir}/main.scss`],
-          modified: "2018-01-01T00:00:00.000Z",
-        },
-        [`${dir}/_colors.scss`]: {
-          imports: [],
-          importedBy: [`${dir}/_footer.scss`, `${dir}/_header.scss`],
-          modified: "2018-01-01T00:00:00.000Z",
-        },
-        [`${dir}/_footer.scss`]: {
-          imports: [`${dir}/_colors.scss`],
-          importedBy: [`${dir}/main.scss`],
-          modified: "2018-01-01T00:00:00.000Z",
-        },
-        [`${dir}/_header.scss`]: {
-          imports: [`${dir}/_colors.scss`],
-          importedBy: [`${dir}/main.scss`],
-          modified: "2018-01-01T00:00:00.000Z",
-        },
-        [`${dir}/main.scss`]: {
-          imports: [
-            `${dir}/_base.scss`,
-            `${dir}/_header.scss`,
-            `${dir}/_footer.scss`,
-          ],
-          importedBy: [],
-          modified: "2018-01-01T00:00:00.000Z",
-        },
+      [`${dir}/_colors.scss`]: {
+        imports: [],
+        importedBy: [`${dir}/_footer.scss`, `${dir}/_header.scss`],
+        modified: "2018-01-01T00:00:00.000Z",
       },
-      loadPaths: ["path/to/load"],
-      addFile: () => {},
-      visitAncestors: () => {},
-      visitDescendents: () => {},
-      visit: () => {},
-    };
+      [`${dir}/_footer.scss`]: {
+        imports: [`${dir}/_colors.scss`],
+        importedBy: [`${dir}/main.scss`],
+        modified: "2018-01-01T00:00:00.000Z",
+      },
+      [`${dir}/_header.scss`]: {
+        imports: [`${dir}/_colors.scss`],
+        importedBy: [`${dir}/main.scss`],
+        modified: "2018-01-01T00:00:00.000Z",
+      },
+      [`${dir}/main.scss`]: {
+        imports: [
+          `${dir}/_base.scss`,
+          `${dir}/_header.scss`,
+          `${dir}/_footer.scss`,
+        ],
+        importedBy: [],
+        modified: "2018-01-01T00:00:00.000Z",
+      },
+    },
+    loadPaths: ["path/to/load"],
+    addFile: () => {},
+    visitAncestors: () => {},
+    visitDescendents: () => {},
+    visit: () => {},
+  };
 
-    const graph = sassGraphGraphToGraph(sassGraphWithIndex);
+  const graph = sassGraphGraphToGraph(sassGraphWithIndex);
 
-    const expectedGraph = new Graph();
-    expectedGraph.addVertice("main", "_base");
-    expectedGraph.addVertice("_footer", "_colors");
-    expectedGraph.addVertice("_header", "_colors");
-    expectedGraph.addVertice("main", "_footer");
-    expectedGraph.addVertice("main", "_header");
-    expect(graph).toEqual(expectedGraph);
-  });
+  const expectedGraph = new Graph();
+  expectedGraph.addVertice("main", "_base");
+  expectedGraph.addVertice("_footer", "_colors");
+  expectedGraph.addVertice("_header", "_colors");
+  expectedGraph.addVertice("main", "_footer");
+  expectedGraph.addVertice("main", "_header");
+  expect(graph).toEqual(expectedGraph);
 });
