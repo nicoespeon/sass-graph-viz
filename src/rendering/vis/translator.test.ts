e3aae70f74063d7bfc5c98206d111f26d28d1909
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-09-19 22:00:12 -0400

    Use different colors for files & partials

--- src/rendering/vis/translator.test.ts
+++ src/rendering/vis/translator.test.ts
@@ -19,11 +19,39 @@ it("translate a graph with some vertices", () => {
 
   const expectedVisGraph = {
     nodes: [
-      { id: "main", label: "main" },
-      { id: "_header", label: "_header" },
-      { id: "_footer", label: "_footer" },
+      expect.objectContaining({ id: "main", label: "main" }),
+      expect.objectContaining({ id: "_header", label: "_header" }),
+      expect.objectContaining({ id: "_footer", label: "_footer" }),
     ],
     edges: [{ from: "main", to: "_header" }, { from: "main", to: "_footer" }],
   };
   expect(visGraph).toEqual(expectedVisGraph);
+});
+
+it("sets proper colors to files & partials", () => {
+  const graph = new Graph();
+  graph.addVertice("main", "_header");
+  graph.addVertice("main", "_footer");
+  graph.addVertice("mobile", "_header");
+  graph.addVertice("_header", "vars/_colors");
+
+  const visGraph = graphToVisGraph(graph);
+
+  const expectedFileColor = {
+    background: "#FF9392",
+    border: "#FF2E2C",
+    highlight: { background: "#FF9392", border: "#FF2E2C" },
+  };
+  const expectedPartialColor = { background: "#D2E5FF" };
+  const expectedNodes = [
+    expect.objectContaining({ id: "main", color: expectedFileColor }),
+    expect.objectContaining({ id: "_header", color: expectedPartialColor }),
+    expect.objectContaining({ id: "_footer", color: expectedPartialColor }),
+    expect.objectContaining({ id: "mobile", color: expectedFileColor }),
+    expect.objectContaining({
+      id: "vars/_colors",
+      color: expectedPartialColor,
+    }),
+  ];
+  expect(visGraph.nodes).toEqual(expectedNodes);
 });
--- src/rendering/vis/translator.ts
+++ src/rendering/vis/translator.ts
@@ -1,8 +1,16 @@
 import { Node, Edge } from "vis";
+import * as path from "path";
 
 import { Graph } from "../../graph";
 
 type VisGraph = { nodes: Node[]; edges: Edge[] };
+
+const fileColor: Node["color"] = {
+  background: "#FF9392",
+  border: "#FF2E2C",
+  highlight: { background: "#FF9392", border: "#FF2E2C" },
+};
+const partialColor: Node["color"] = { background: "#D2E5FF" };
 
 export function graphToVisGraph(graph: Graph): VisGraph {
   const nodes: Node[] = [];
@@ -12,15 +20,28 @@ export function graphToVisGraph(graph: Graph): VisGraph {
     const nodeIds = nodes.map(({ id }) => id);
 
     if (!nodeIds.includes(parent)) {
-      nodes.push({ id: parent, label: parent });
+      nodes.push({
+        id: parent,
+        label: parent,
+        color: isPartial(parent) ? partialColor : fileColor,
+      });
     }
 
     if (!nodeIds.includes(child)) {
-      nodes.push({ id: child, label: child });
+      nodes.push({
+        id: child,
+        label: child,
+        color: isPartial(child) ? partialColor : fileColor,
+      });
     }
 
     edges.push({ from: parent, to: child });
   });
 
   return { nodes, edges };
+}
+
+// TODO: move this up to Grapsh domain
+function isPartial(node: string): boolean {
+  return path.basename(node).startsWith("_");
 }
