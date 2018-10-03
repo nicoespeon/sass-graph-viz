a0988fb677381b471d6a4558137ab6ca90559a4a
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-10-03 00:40:44 -0400

    Add ability to focus on a node

--- src/graph.test.ts
+++ src/graph.test.ts
@@ -19,3 +19,36 @@ it("should return graph edges", () => {
     ["_colors", "_variables"],
   ]);
 });
+
+describe("focus on a node", () => {
+  const nodeName = "_footer";
+
+  it("should filter out nodes that are not linked to the given one", () => {
+    const graph = new Graph();
+    graph.addEdge("main", "_base");
+    graph.addEdge("main", "_header");
+    graph.addEdge("_base", nodeName);
+    graph.addEdge("_header", "_colors");
+    graph.addEdge(nodeName, "_colors");
+    graph.addEdge("_colors", "_variables");
+
+    const filteredGraph = graph.focusOnNode(nodeName);
+
+    const expectedGraph = new Graph();
+    expectedGraph.addEdge("main", "_base");
+    expectedGraph.addEdge("_base", nodeName);
+    expectedGraph.addEdge(nodeName, "_colors");
+    expectedGraph.addEdge("_colors", "_variables");
+    expect(filteredGraph).toEqual(expectedGraph);
+  });
+
+  it("should handle cyclic dependencies", () => {
+    const graph = new Graph();
+    graph.addEdge("main", nodeName);
+    graph.addEdge(nodeName, "_colors");
+    graph.addEdge("_colors", "_variables");
+    graph.addEdge("_variables", "_colors");
+
+    expect(() => graph.focusOnNode(nodeName)).not.toThrow();
+  });
+});
--- src/graph.ts
+++ src/graph.ts
@@ -19,6 +19,51 @@ export class Graph {
 
     return edges;
   }
+
+  focusOnNode(nodeName: string): Graph {
+    const graph = new Graph();
+
+    this.getEdges().forEach(([parent, child]) => {
+      if (
+        this.isAncestorOrDescendent(parent.name, nodeName) &&
+        this.isAncestorOrDescendent(child.name, nodeName)
+      ) {
+        graph.addEdge(parent.name, child.name);
+      }
+    });
+
+    return graph;
+  }
+
+  private isAncestorOrDescendent(node: string, target: string): boolean {
+    return this.isAncestor(node, target) || this.isDescendent(node, target);
+  }
+
+  private isAncestor(node: string, target: string): boolean {
+    return this.isDescendent(target, node);
+  }
+
+  private isDescendent(
+    node: string,
+    target: string,
+    visitedTargets: string[] = [],
+  ): boolean {
+    // Don't visit twice a target for performance. Avoid blowing call stack.
+    if (visitedTargets.includes(target)) return false;
+    visitedTargets.push(target);
+
+    if (node === target) return true;
+
+    const targetChildren = this.graph[target];
+    if (!targetChildren) return false;
+
+    return (
+      targetChildren.includes(node) ||
+      targetChildren.some((child) =>
+        this.isDescendent(node, child, visitedTargets),
+      )
+    );
+  }
 }
 
 class Node {
