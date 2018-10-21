7ca6bceeeb9d42dcedd18ceaeab2ff40862641f8
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-10-20 23:01:23 -0400

    Fix example without externals

--- examples/without-externals/index.ts
+++ examples/without-externals/index.ts
@@ -2,5 +2,5 @@ import * as path from "path";
 import { generateVisualGraph } from "../../src";
 
 generateVisualGraph(path.join(__dirname, "fixtures", "target"), {
-  excludeExternals: false,
+  excludeExternals: true,
 });
